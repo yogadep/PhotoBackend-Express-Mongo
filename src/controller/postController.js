05824cadad7ad87c7e60341d2982790c9bef342e
yogadep <yoghaap7@gmail.com>
2024-05-24 00:42:53 +0700

    fix populate in postController

--- src/controller/postController.js
+++ src/controller/postController.js
@@ -2,7 +2,7 @@ import Post from "../model/post.js";
 
 export const getAllPosts = async (req, res, next)=>{
     try {
-        const posts = await Post.find().populate('createdBy', 'nama')
+        const posts = await Post.find().populate('createdBy', 'name')
         res.status(200).json({ posts })
     } catch (error) {
         next(error)
--- src/middleeware/uploadPic.js
+++ src/middleeware/uploadPic.js
@@ -2,10 +2,10 @@ import multer from "multer";
 import path from "path";
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-import fs from 'fs';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb)=>{
