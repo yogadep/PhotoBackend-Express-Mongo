23598f3a505221dfd722e9e8138e6eda8af82837
yogadep <yoghaap7@gmail.com>
2024-06-06 20:44:18 +0700

    edit deletePost

--- src/controller/postController.js
+++ src/controller/postController.js
@@ -71,9 +71,6 @@ export const updatePost = async (req, res, next) => {
 export const deletePost = async (req, res, next) => {
     const { id } = req.params;
     try {
-        if(!id){
-            return res.status(400).json({ error : "Id tidak ditemukan" })
-        } 
         const delPost = await Post.findByIdAndDelete({ _id: id })
         return res.status(200).json({ message: "Berhasil menghapus konten", delPost })
     } catch (error) {
