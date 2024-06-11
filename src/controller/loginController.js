16cd227ac5a1bf54adb612e89f048ba1a1502bf1
yogadep <yoghaap7@gmail.com>
2024-06-11 15:59:53 +0700

    editing code in verifikasiToken

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -7,20 +7,6 @@ export const login = async (req, res, next) => {
     const {email, password} = req.body;
 
     try {
-        // let user = await Admin.findOne({ email });
-        
-        // if(!user || !bcrypt.compareSync(password, user.password)){
-        //     res.status(400).json({error: "username/password salah"})
-        // }else{
-        //     const token = Jwt.sign(
-        //         { 
-        //             userId: user._id,
-        //             username: user.username 
-        //         }, 
-        //         process.env.secret,
-        //         { expiresIn: '1h' });
-        //     res.status(200).json({token})
-        // }
         let user = await Admin.findOne({ email });
 
         if(user && bcrypt.compareSync(password, user.password)){
