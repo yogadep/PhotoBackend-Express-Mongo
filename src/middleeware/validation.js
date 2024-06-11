78f22143daa4a0620d76e6046f2888375221ddea
yogadep <yoghaap7@gmail.com>
2024-06-11 17:31:22 +0700

    moving postValidator to validator Middleware

--- src/middleeware/validation.js
+++ src/middleeware/validation.js
@@ -1,11 +1,63 @@
+import { body, check } from "express-validator";
 import { validationResult } from "express-validator";
 
-const validate = (req, res, next) => {
+export const postValidation = [
+    body('title')
+        .exists({ checkFalsy: true })
+        .withMessage('title wajib diisi')
+        .isString()
+        .withMessage('title harus string'),
+    body('content')
+        .exists({ checkFalsy: true })
+        .withMessage('content wajib diisi')
+        .isString()
+        .withMessage('content harus string')
+        .isLength({ min: 5 })
+        .withMessage("content minimal 5 karakter"),
+    check('image')
+        .custom((value, {req}) => {
+            // console.log("File yang diunggah di validasi:", req.file); 
+            if(!req.file){
+                throw new Error('Silahkan upload gambar terlebih dulu')
+            }
+
+            const allowedExtensions = ['image/jpeg', 'image/png'];
+            // mime : file extensions
+            if (!allowedExtensions.includes(req.file.mimetype)) {
+                throw new Error('Hanya file JPG atau PNG yang diperbolehkan');
+            }
+            return true;
+        })
+]
+
+export const updateValidation = [
+    body('title')
+        .optional()
+        .isLength({ min: 5, max: 20 })
+        .withMessage('Panjang konten antara 5-20 karakter'),
+    body('content')
+        .optional()
+        .isLength({ min: 5, max: 20 })
+        .withMessage('Panjang konten antara 5-20 karakter'),
+    check('image')
+        .custom((value, {req}) => {
+		   if(!req.file){
+				return true;
+		   }
+
+           const allowedExtensions = ['image/jpeg', 'image/png']; 
+
+           if (!allowedExtensions.includes(req.file.mimetype)) {
+           		throw new Error('Hanya file JPG atau PNG yang diperbolehkan');
+           }
+		   return true;
+        })
+]
+
+export const validate = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
     next();
 };
-
-export default validate
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -9,64 +9,10 @@ import
     } from "../controller/postController.js";
 import { verifyToken } from "../middleeware/verifikasiToken.js";
 import { upload } from "../middleeware/uploadPic.js";
-import { body, check } from "express-validator";
-import validate from "../middleeware/validation.js"
-import { checkRole } from "../controller/checkRole.js";
+import { checkRole } from "../middleeware/checkRole.js";
+import { postValidation, updateValidation, validate } from "../middleeware/validation.js";
 
 const postRouter = Router()
-// post validation -> validation middleware
-const postValidation = [
-    body('title')
-        .exists({ checkFalsy: true })
-        .withMessage('title wajib diisi')
-        .isString()
-        .withMessage('title harus string'),
-    body('content')
-        .exists({ checkFalsy: true })
-        .withMessage('content wajib diisi')
-        .isString()
-        .withMessage('content harus string')
-        .isLength({ min: 5 })
-        .withMessage("content minimal 5 karakter"),
-    check('image')
-        .custom((value, {req}) => {
-            // console.log("File yang diunggah di validasi:", req.file); 
-            if(!req.file){
-                throw new Error('Silahkan upload gambar terlebih dulu')
-            }
-
-            const allowedExtensions = ['image/jpeg', 'image/png'];
-            // mime : file extensions
-            if (!allowedExtensions.includes(req.file.mimetype)) {
-                throw new Error('Hanya file JPG atau PNG yang diperbolehkan');
-            }
-            return true;
-        })
-]
-
-const updateValidation = [
-    body('title')
-        .optional()
-        .isLength({ min: 5, max: 20 })
-        .withMessage('Panjang konten antara 5-20 karakter'),
-    body('content')
-        .optional()
-        .isLength({ min: 5, max: 20 })
-        .withMessage('Panjang konten antara 5-20 karakter'),
-    check('image')
-        .custom((value, {req}) => {
-		   if(!req.file){
-				return true;
-		   }
-
-           const allowedExtensions = ['image/jpeg', 'image/png']; 
-
-           if (!allowedExtensions.includes(req.file.mimetype)) {
-           		throw new Error('Hanya file JPG atau PNG yang diperbolehkan');
-           }
-		   return true;
-        })
-]
 
 postRouter
     .get('/', getAllPosts)
