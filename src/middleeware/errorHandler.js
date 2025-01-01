0bfce6b49849699c78421a32774ed136b17533cd
yogadep <yoghaap7@gmail.com>
2025-01-01 22:37:45 +0700

    Update changes

--- src/middleeware/errorHandler.js
+++ src/middleeware/errorHandler.js
@@ -1,5 +1,6 @@
 export const errorHandler = (err, req, res, next) => {
     // console.error(err.stack); 
+    // up
     res.status(err.status || 500).json({
         error: {
             message: 'Terjadi kesalahan server',
